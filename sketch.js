e55a31416da76c97c25d9a6a1cb2c2737fe86a9d
Park Jieun <olahola16@snu.ac.kr>
2025-05-23 16:47:47 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400, 400);
+  textSize(24);
+  textAlign(CENTER,CENTER);
+}
+
+function draw() {
+  background(220);
+  text('github 배포 테스트', width/2, height/2);
+}
